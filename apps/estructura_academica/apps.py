0ee943d71b9ac9b671c17bc912c9511c35bc53eb
Alvaro Encinas <AlvaroEncinas26@gmail.com>
2026-04-15 23:19:31 -0400

    base estable

---
+++ apps/estructura_academica/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EstructuraAcademicaConfig(AppConfig):
+    name = 'apps.estructura_academica'
---
+++ apps/reordenamiento/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ReordenamientoConfig(AppConfig):
+    name = 'apps.reordenamiento'
